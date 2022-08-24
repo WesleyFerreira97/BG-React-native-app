892c53d276d56e5cf8d336e84291bf33e4c9413d
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-08-24 02:30:09 -0300

    refact: component image input

--- src/components/ImageInput/index.tsx
+++ src/components/ImageInput/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, View, Text } from 'react-native';
 import { Button } from 'react-native-paper';
 import * as ImagePicker from 'expo-image-picker';
 import { styles } from './styles';
 import { useField } from 'formik';
+import { supaDb } from '../../services/supadb';
 
 export function ImageInput({ label, ...props }: any) {
     const [imageSrc, setImageSrc] = useState<string | null>(null);
@@ -18,10 +19,25 @@ export function ImageInput({ label, ...props }: any) {
         });
 
         if (!result.cancelled) {
+            console.log(result, "base64 filho");
+
             setImageSrc(result.uri);
             helpers.setValue(result.uri)
         }
     };
+
+    // useEffect(() => {
+    //     if (!imageSrc) return;
+
+    //     supaDb.storage
+    //         .from("photo")
+    //         .upload("public/news/image1.jpg", imageSrc)
+    //         .then((res) => {
+    //             console.log(res);
+
+    //         });
+
+    // }, [imageSrc]);
 
     return (
         <View style={styles.container}>
@@ -37,6 +53,7 @@ export function ImageInput({ label, ...props }: any) {
             {meta.touched && meta.error ? (
                 <Text style={{ color: "red" }}>{meta.error}</Text>
             ) : null}
+
         </View>
     );
 }
