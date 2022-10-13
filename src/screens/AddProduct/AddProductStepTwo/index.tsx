05cb34d952b6cb7229111c665ccaca67178aa16e
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-10-13 17:54:40 -0300

    added header to section colors

--- src/screens/AddProduct/AddProductStepTwo/index.tsx
+++ src/screens/AddProduct/AddProductStepTwo/index.tsx
@@ -1,4 +1,5 @@
-import { FieldArray, Formik } from 'formik';
+import { FieldArray, Formik, useFormik } from 'formik';
+import { FilePlus } from 'phosphor-react-native';
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { Button } from 'react-native-paper';
@@ -13,6 +14,7 @@ import { theme } from '../../../styles/theme';
 import { styles } from './styles';
 
 export function AddProductStepTwo({ route }) {
+    // const { } = useFormik({})
     const { selectResponse, selectResponseError } = useSelect<BucketProps>({
         select: ['bucket_name', 'bucket_folder'],
         match: route.params.productId,
@@ -46,14 +48,32 @@ export function AddProductStepTwo({ route }) {
                         onSubmit={values => handleSubmit(values)}
 
                     >
-                        {({ handleChange, handleBlur, handleSubmit, values }) => (
+                        {({ handleChange, handleBlur, handleSubmit, values, }) => (
                             <>
-
                                 <View style={styles.form}>
                                     <Modal label="Adicionar nova Cor" >
-                                        <Text>Conteudo de texto</Text>
+                                        <Text>Selecione uma cor : </Text>
+                                        <Button>Azul</Button>
+                                        <Button onPress={() => console.log('Vemelho')}>Vermelho</Button>
+                                        <Button>Branco</Button>
                                     </Modal>
+
+                                    <View style={styles.galleryByColor}>
+                                        <View style={styles.galleryHeader}>
+                                            <View style={styles.galleryOptions}>
+                                                <FilePlus
+                                                    size={32}
+                                                    style={{ marginRight: 5 }}
+                                                    color={theme.colors.neutralAlt}
+                                                />
+                                                <Text style={styles.galleryTitle}>Editar</Text>
+                                            </View>
+                                            <Text style={styles.galleryTitle}>Rosa</Text>
+                                        </View>
+                                    </View>
+
                                 </View>
+
                                 <View style={styles.footer}>
                                     <Button
                                         onPress={handleSubmit}
--- src/screens/AddProduct/AddProductStepTwo/styles.ts
+++ src/screens/AddProduct/AddProductStepTwo/styles.ts
@@ -16,5 +16,26 @@ export const styles = StyleSheet.create({
     },
     footer: {
         marginVertical: 10,
+    },
+    galleryByColor: {
+        backgroundColor: theme.colors.neutral,
+    },
+    galleryHeader: {
+        justifyContent: 'space-between',
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: theme.colors.primary,
+        borderTopEndRadius: 5,
+        borderTopStartRadius: 5,
+        padding: 7,
+    },
+    galleryTitle: {
+        color: theme.colors.neutral,
+        fontSize: 14,
+        fontFamily: theme.fonts.secondaryAlt
+    },
+    galleryOptions: {
+        flexDirection: "row",
+        alignItems: "center",
     }
 });
