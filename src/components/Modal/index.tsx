1df002f8ee625bc9eb33de91b8225a2a0c06c0c1
Wesley Ferreira <wesleyfc00@hotmail.com>
2022-10-14 20:35:13 -0300

    Modal changes, need fix state shared

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -8,47 +8,58 @@ type ModalProps = {
     value?: string | number,
 }
 
-export function Modal({ children, label, ...props }: ModalProps) {
+const ModalContent = () => {
+
+    return (
+        <ModalNative
+            animationType="fade"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+                setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.modalOverlay} />
+            <View style={styles.container}>
+                <View style={styles.modalWrap}>
+
+                    <View style={styles.modalContent}>
+                        {children}
+                    </View>
+
+                    <View style={styles.modalFooter}>
+                        <TouchableOpacity onPress={() => setModalVisible(false)}>
+                            <View style={styles.closeButton}>
+                                <Text>Fechar</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+
+        </ModalNative>
+    )
+}
+
+const ModalButton = () => {
     const [modalVisible, setModalVisible] = useState(false);
 
     return (
-        <>
-            <ModalNative
-                animationType="fade"
-                transparent={true}
-                visible={modalVisible}
-                onRequestClose={() => {
-                    setModalVisible(!modalVisible);
-                }}>
-                <View style={styles.modalOverlay} />
-                <View style={styles.container}>
-                    <View style={styles.modalWrap}>
-
-                        <View style={styles.modalContent}>
-                            {children}
-                        </View>
-
-                        <View style={styles.modalFooter}>
-                            <TouchableOpacity onPress={() => setModalVisible(false)}>
-                                <View style={styles.closeButton}>
-                                    <Text>Fechar</Text>
-                                </View>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
-
-            </ModalNative>
-
-
-            <TouchableOpacity
-                onPress={() => setModalVisible(true)}
-                style={styles.labelContainer}
-            >
-                <Text style={styles.label}>
-                    {label} &nbsp; {props.value}
-                </Text>
-            </TouchableOpacity>
-        </>
-    );
+        <TouchableOpacity
+            onPress={() => setModalVisible(true)}
+            style={styles.labelContainer}
+        >
+            <Text style={styles.label}>
+                {label} &nbsp; {props.value}
+            </Text>
+        </TouchableOpacity>
+    )
 }
+
+function Modal({ children, label, ...props }: ModalProps) {
+    return { children };
+}
+
+Modal.ModalContent = ModalContent;
+Modal.ModalButton = ModalButton;
+
+export { Modal };
