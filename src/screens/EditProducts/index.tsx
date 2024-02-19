52234397bdbd169f003fcfcdadde3b7a52171cf7
Wesley Ferreira <wesleyferreira97@outlook.com>
2024-02-18 21:29:16 -0300

    feat: fetch current product to edit in screen EditProducts

--- src/screens/EditProducts/index.tsx
+++ src/screens/EditProducts/index.tsx
@@ -1,21 +1,44 @@
 import { Text, View } from 'react-native';
-
 import { styles } from './styles';
-import { useNavigation, useRoute } from '@react-navigation/native';
-import { useEffect } from 'react';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
+import { useSelect } from '../../hooks/useSelect';
+import type { AllProductProps, ProductProps } from '../../@types/product';
+import { TextInput } from '../../components/TextInput';
+import { Formik } from 'formik';
 
 type EditProps = {
     itemId: string
 }
 
-export function EditProducts(props: EditProps) {
-    const route = useRoute();
+export function EditProducts() {
+    const { params }: RouteProp<{ params: EditProps }> = useRoute();
 
-    console.log(route.params);
+    // const { selectResponse, selectResponseError } = useSelect<AllProductProps[]>({
+    //     tableName: 'products',
+    //     select: ['title', 'bucket_name', 'bucket_folder', 'id'],
+    //     limit: 3,
+    //     match: route.params.itemId
+    // });
+
+    // console.log(route.params);
+
+    const initialValues = {
+        title: params.itemId
+    }
 
     return (
         <View style={styles.container}>
             <Text>Pedidos Screen</Text>
+            <Formik
+                initialValues={initialValues}
+                // validationSchema={productValidation}
+                onSubmit={(values: ProductProps) => {
+                    // handleSubmitProduct(values);
+                }} >
+                {({ handleChange, handleBlur, handleSubmit, values, errors, touched, submitForm }) => (
+                    <TextInput name='title' label='Título' />
+                )}
+            </Formik>
         </View>
     );
 }
