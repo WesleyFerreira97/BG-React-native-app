2719ced670fd7b2498d4811032a402d775c612e7
Wesley Ferreira <wesleyfc00@hotmail.com>
2024-05-20 15:54:39 -0300

    feat: hook useGallery

---
+++ src/hooks/useGallery.ts
@@ -0,0 +1,74 @@
+// Gerenciar as sessões ativas de galerias ( main, blue, purple etc...)
+
+import { useState } from "react";
+import { sectionColors, SectionColorsNames, SectionColorsProps } from "../screens/AddProduct/AddProductStepTwo/sectionColors";
+
+type SectionProps = {
+    images: any[];
+} & SectionColorsProps;
+
+export function useGallery() {
+    const [gallerySections, setGallerySections] = useState<SectionProps[]>([]);
+    const [error, setError] = useState<any>(null);
+
+    const handleNewSection = (sectionName: SectionColorsNames) => {
+        const noActiveSections = (gallerySections.length === 0);
+
+        if (noActiveSections) return setGallerySections(prev => [...prev, {
+            ...sectionColors[sectionName],
+            images: []
+        }]);
+
+        const isDuplicate = gallerySections.some(
+            section => section.name === sectionName
+        );
+
+        if (isDuplicate) return setError({
+            state: true,
+            text: "Seção Já adicionada"
+        });
+
+        setGallerySections(prev => [...prev, {
+            ...sectionColors[sectionName],
+            images: []
+        }]);
+    }
+
+    const addImages = (sectionName: SectionColorsNames, images: any[]) => {
+        const sectionIndex = gallerySections.findIndex(
+            section => section.name === sectionName
+        );
+
+        if (sectionIndex === -1) return setError({
+            state: true,
+            text: "Seção não encontrada"
+        });
+
+        setGallerySections(prev => {
+            const newGallerySections = [...prev];
+            const currentImages = newGallerySections[sectionIndex].images;
+
+            newGallerySections[sectionIndex].images = [...currentImages, ...images];
+            return newGallerySections;
+        });
+    }
+
+    return { handleNewSection, gallerySections, addImages, error }
+}
+
+// handleNewSection('blue')
+// setImages('blue', [image1, image2, image3])
+// Deve retornar ;
+// Função para adicionar Galeria nova handleNewSection
+// Lista de galerias ativas currentGallerySections e suas respectivas Imagens
+// Função para adicionar imagens a galerias especificas setImages
+
+
+const gallerySections = {
+    blue: {
+        name: "Azul",
+        slug: "blue",
+        colorCode: "#083AA9",
+        images: []
+    }
+}
--- src/screens/AddProduct/AddProductStepTwo/sectionColors.ts
+++ src/screens/AddProduct/AddProductStepTwo/sectionColors.ts
@@ -3,14 +3,14 @@ type SectionColorsNames = 'blue' | 'white' | 'purple';
 type SectionColorsProps = {
     slug: string;
     name: string;
-    color: string;
+    colorCode: string;
 }
 
 type f = {
     [key in SectionColorsNames]: {
         slug: string,
         name: string,
-        color: string
+        colorCode: string
     }
 }
 
@@ -18,17 +18,17 @@ const sectionColors: Record<SectionColorsNames, SectionColorsProps> = {
     blue: {
         slug: 'blue',
         name: 'Azul',
-        color: '#083AA9'
+        colorCode: '#083AA9'
     },
     white: {
         slug: 'white',
         name: "Branco",
-        color: '#B2B2B2',
+        colorCode: '#B2B2B2',
     },
     purple: {
         slug: 'purple',
         name: "Roxo",
-        color: '#3F3351',
+        colorCode: '#3F3351',
     }
 }
 
